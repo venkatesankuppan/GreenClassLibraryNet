66d366dd7bb870c80169c2dda93944f8711b2dc1
venkatesan kuppan <netvenkat@live.com>
2017-11-13 19:15:58 +0530

    first commit

---
+++ ClassLibrary1/TestClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GreenClassLibraryDevops;
+
+namespace GreenClass.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void AddTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Add(20, 10);
+            Assert.AreEqual(30, result);
+        }
+
+        [TestCase]
+        public void SubtractTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Subtract(20, 10);
+            Assert.AreEqual(10, result);
+        }
+    }
+
+
+
+}
---
+++ GreenClassLibraryDevops/Maths.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenClassLibraryDevops
+{
+  class Maths
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Enter two numbers\n");
+                int number1;
+                int number2;
+                number1 = int.Parse(Console.ReadLine());
+                number2 = int.Parse(Console.ReadLine());
+
+                MathsHelper helper = new MathsHelper();
+                int x = helper.Add(number1, number2);
+                Console.WriteLine("\nThe sum of " + number1 +
+                    " and " + number2 + " is " + x);
+                Console.ReadKey();
+                int y = helper.Subtract(number1, number2);
+                Console.WriteLine("\nThe difference between " +
+                      number1 + " and" + number2 + "  is " + y);
+                Console.ReadKey();
+            }
+            catch(InsufficientMemoryException ex)
+            {
+
+
+
+
+
+
+            }
+        }
+    }
+
+    public class MathsHelper
+    {
+        public MathsHelper() { }
+        public int Add(int a, int b)
+        {
+            int x = a + b;
+            return x;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            int x = a - b;
+            return x;
+        }
+    }
+}
